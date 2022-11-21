0ec428aa63c147a5b06983976d187a349eb02f55
Andrey <basmsk@mail.ru>
2022-11-21 21:27:22 +0500

    numbers and strings

---
+++ script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// const str = 'test';
+// console.log(str.length);
+
+const arr = [1, 2, 3, 4, 5];
+console.log(arr.length);
+
+const str = 'tEst';
+console.log(str.toUpperCase());
+console.log(str);
+console.log(str.toLowerCase());
+
+const fruit = 'Some fruit';
+
+console.log(fruit.indexOf('Some'));
+
+const logg = "Hi bro! No bro!";
+console.log(logg.slice(7, 13));
+
+console.log(logg.substring(4, 8));
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const testt = "12.2px";
+console.log(parseInt(testt));
+console.log(parseFloat(testt));
